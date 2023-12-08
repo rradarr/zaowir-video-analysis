9337dc62183c70e712927f0acce59c9f40095f24
rradarr <radorze093@student.polsl.pl>
2023-12-08 12:26:14 +0100

    Add contents of lab2
    The program can perform stereo calibration and rectification of
    stereo image data as well as save a visualization of the effects
    of remapping with epipolar lines.

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,3 +1,4 @@
+import time
 from typing import List, Tuple
 import cv2, cv2.typing
 import os
@@ -189,7 +190,114 @@ def load_and_rectify_stereo(in_file_name: str,
         cv2.imwrite(f'{image_left.file_path.stem}_rectified.png', rect_left)
         cv2.imwrite(f'{image_right.file_path.stem}_rectified.png', rect_right)
 
+def remap_test(in_file_name: str,
+                dataset_left_name_pattern: str,
+                dataset_right_name_pattern: str):
+    image_file_names_left = glob.glob(dataset_left_name_pattern)
+    image_file_names_right = glob.glob(dataset_right_name_pattern)
+
+    remap_methods = [cv2.INTER_NEAREST, cv2.INTER_LINEAR, cv2.INTER_CUBIC, cv2.INTER_AREA, cv2.INTER_LANCZOS4]
+
+    # Get a sample image to determine the shape of images
+    sample_image = Image(image_file_names_left[0])
+    image_size = sample_image.bw_data.shape[::-1]
+
+    # Load the calibration data
+    with open(in_file_name, "r") as calibration_file:
+        calibration_json = json.load(calibration_file)
+        camera_mat_left = numpy.array(calibration_json["camera_mat_left"])
+        distortion_left = numpy.array(calibration_json["distortion_left"])
+        camera_mat_right = numpy.array(calibration_json["camera_mat_right"])
+        distortion_right = numpy.array(calibration_json["distortion_right"])
+        rotation_mat = numpy.array(calibration_json["rotation_mat"])
+        translation_vec = numpy.array(calibration_json["translation_vec"])
+        essential_mat = numpy.array(calibration_json["essential_mat"])
+        fundamental_mat = numpy.array(calibration_json["fundamental_mat"])
+
+    for name_left, name_right in zip(image_file_names_left, image_file_names_right):
+        # Get the images
+        image_left = Image(name_left)
+        image_right = Image(name_right)
+
+        rect_left, rect_right, proj_left, proj_right, disparity_to_depth_map, roi_left, roi_right = cv2.stereoRectify(
+            camera_mat_left, distortion_left, camera_mat_right, distortion_right,
+            image_size, rotation_mat, translation_vec
+        )
+        map_left_x, map_left_y = cv2.initUndistortRectifyMap(
+            camera_mat_left, distortion_left, rect_left, proj_left, image_size, cv2.CV_32FC1 # type: ignore
+        )
+        map_right_x, map_right_y = cv2.initUndistortRectifyMap(
+            camera_mat_right, distortion_right, rect_right, proj_right, image_size, cv2.CV_32FC1 # type: ignore
+        )
+
+        for remap_method in remap_methods:
+            start_time = time.time()
+            rect_left = cv2.remap(image_left.bw_data, map_left_x, map_left_y, remap_method)
+            rect_right = cv2.remap(image_right.bw_data, map_right_x, map_right_y, remap_method)
+            print(f'rm{remap_method}: {round(time.time() - start_time, 5)} seconds')
+
+            cv2.imwrite(f'{image_left.file_path.stem}_rectified_rm{remap_method}.png', rect_left)
+            cv2.imwrite(f'{image_right.file_path.stem}_rectified_rm{remap_method}.png', rect_right)
+
+def load_rectified_and_draw_epi(in_file_name: str,
+                dataset_left_name_pattern: str,
+                dataset_right_name_pattern: str):
+    image_file_names_left = glob.glob(dataset_left_name_pattern)
+    image_file_names_right = glob.glob(dataset_right_name_pattern)
+
+    # Get a sample image to determine the shape of images
+    sample_image = Image(image_file_names_left[0])
+    image_size = sample_image.bw_data.shape[::-1]
+
+    # Load the calibration data
+    with open(in_file_name, "r") as calibration_file:
+        calibration_json = json.load(calibration_file)
+        camera_mat_left = numpy.array(calibration_json["camera_mat_left"])
+        distortion_left = numpy.array(calibration_json["distortion_left"])
+        camera_mat_right = numpy.array(calibration_json["camera_mat_right"])
+        distortion_right = numpy.array(calibration_json["distortion_right"])
+        rotation_mat = numpy.array(calibration_json["rotation_mat"])
+        translation_vec = numpy.array(calibration_json["translation_vec"])
+        essential_mat = numpy.array(calibration_json["essential_mat"])
+        fundamental_mat = numpy.array(calibration_json["fundamental_mat"])
+
+    for name_left, name_right in zip(image_file_names_left, image_file_names_right):
+        # Get the images
+        image_left = Image(name_left)
+        image_right = Image(name_right)
+
+        rect_left, rect_right, proj_left, proj_right, disparity_to_depth_map, roi_left, roi_right = cv2.stereoRectify(
+            camera_mat_left, distortion_left, camera_mat_right, distortion_right,
+            image_size, rotation_mat, translation_vec
+        )
+        map_left_x, map_left_y = cv2.initUndistortRectifyMap(
+            camera_mat_left, distortion_left, rect_left, proj_left, image_size, cv2.CV_32FC1 # type: ignore
+        )
+        map_right_x, map_right_y = cv2.initUndistortRectifyMap(
+            camera_mat_right, distortion_right, rect_right, proj_right, image_size, cv2.CV_32FC1 # type: ignore
+        )
+
+        rect_left = cv2.remap(image_left.bw_data, map_left_x, map_left_y, cv2.INTER_LANCZOS4)
+        rect_right = cv2.remap(image_right.bw_data, map_right_x, map_right_y, cv2.INTER_LANCZOS4)
+
+        cv2.rectangle(rect_left, (roi_left[0], roi_left[1]), (roi_left[2], roi_left[3]), (255, 0, 0), 4)
+        cv2.rectangle(rect_right, (roi_right[0], roi_right[1]), (roi_right[2], roi_right[3]), (255, 0, 0), 4)
+
+        images_together = numpy.concatenate((rect_left, rect_right), axis=1)
+        add_lines(images_together)
+
+        cv2.imwrite(f'{image_right.file_path.stem}_epi_joined.png', images_together)
+
+def add_lines(image):
+    for line in range(0, image.shape[1], 50):
+        cv2.line(image, (0, line), (image.shape[1], line), (0, 0, 255), 2)
+
+
 if __name__ == "__main__":
     #calibrate_stereo_and_save("stereo_params.json", ".\\s3\\left_*.png", ".\\s3\\right_*.png")
 
-    load_and_rectify_stereo("stereo_params.json", ".\\s3\\left_255.png", ".\\s3\\right_255.png")
+    #load_and_rectify_stereo("stereo_params.json", ".\\s3\\left_255.png", ".\\s3\\right_255.png")
+
+    #remap_test("stereo_params.json", ".\\s3\\left_255.png", ".\\s3\\right_255.png")
+
+    load_rectified_and_draw_epi("stereo_params.json", ".\\s3\\left_15.png", ".\\s3\\right_15.png")
